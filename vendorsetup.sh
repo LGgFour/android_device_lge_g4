1d85337fc7e59bafbd71cab848e648b00f74964c
steadfasterX <steadfasterX@gmail.com>
2020-07-23 17:59:10 +0200

    g4: fix wrong OTA URL
    Signed-off-by: steadfasterX <steadfasterX@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -6,9 +6,9 @@ export LLVM_ENABLE_THREADS=1
 #export EOS_BRANCH_NAME="v0.9.3-pie"
 export EOS_DEVICE=h815
 export EOS_BRANCH_NAME="v1-pie"
-export EOS_OTA_URL="http://sfxota.binbash.rocks:8009/e-os/pie"
 export EOS_RELEASE_TYPE="UNOFFICIAL"
 export EOS_CUSTOM_PACKAGES="MuPDF GmsCore GsfProxy FakeStore com.google.android.maps.jar Telegram Signal Mail BlissLauncher BlissIconPack MozillaNlpBackend OpenWeatherMapWeatherProvider AccountManager MagicEarth OpenCamera eDrive Weather Notes Tasks NominatimNlpBackend DroidGuard OpenKeychain Message Browser BrowserWebView Apps LibreOfficeViewer"
+export EOS_OTA_URL="http://sfxota.binbash.rocks:8009/e-os/pie/api"
 export EOS_OTA_CLEARTEXT=true
 
 add_lunch_combo lineage_f500_usu-userdebug
