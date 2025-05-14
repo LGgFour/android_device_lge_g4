7b5f8457047f2feacd54d7c48153d3460fc7686d
steadfasterX <steadfasterX@users.noreply.github.com>
2025-05-14 07:45:56 +0200

    product: add h818

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -26,6 +26,7 @@ PRODUCT_MAKEFILES += \
 	$(LOCAL_DIR)/lineage_h812_usu.mk \
 	$(LOCAL_DIR)/lineage_h815.mk \
 	$(LOCAL_DIR)/lineage_h815_usu.mk \
+        $(LOCAL_DIR)/lineage_h818_usu.mk \
 	$(LOCAL_DIR)/lineage_ls991_usu.mk \
 	$(LOCAL_DIR)/lineage_us991_usu.mk \
 	$(LOCAL_DIR)/lineage_vs986_usu.mk
@@ -49,6 +50,9 @@ COMMON_LUNCH_CHOICES := \
 	lineage_h815_usu-userdebug \
 	lineage_h815_usu-user \
 	lineage_h815_usu-eng \
+        lineage_h818-user \
+	lineage_h818-eng \
+	lineage_h818_usu-userdebug \
 	lineage_ls991_usu-userdebug \
 	lineage_ls991_usu-user \
 	lineage_ls991_usu-eng \
