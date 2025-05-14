8b48ac0995cbbf35b1e3235a613c23b18e12046b
steadfasterX <steadfasterX@users.noreply.github.com>
2025-05-14 07:42:05 +0200

    board: add h818

---
+++ h818_usu/BoardConfig.mk
@@ -0,0 +1,9 @@
+# inherit board config for all g4's
+include device/lge/g4/BoardConfig.mk
+
+# Partitions
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 4341104640
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 24897388544
+
+# Kernel
+TARGET_KERNEL_CONFIG := usu_h818_defconfig
